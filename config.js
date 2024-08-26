bdaf35b783367fabf6ebfad1198469589bd600ba
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-08-26 18:15:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/eypz/1724661875852_gwwMRtTtz.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "> *OCTA ALIVE*",
+OWNER_NAME: process.env.OWNER_NAME || "Eypz",
+BOT_NAME: process.env.BOT_NAME || "OCTA",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
