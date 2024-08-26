e836655b1d4a5af681bce7e909a474453605a1af
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-08-26 19:40:08 +0530

    AddPlugin Was Created 🌸

---
+++ plugins/AddPlugin.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const path = require('path')
+const { eypz, commands } = require('../command')
+
+eypz({
+    pattern: "addplugin",
+    desc: "New Plugin Add To Octa Bot",
+    category: "owner",
+    filename: __filename
+},
+
+
+async(conn, mek, m, { reply, quoted, args }) => {
+    try {
+        
+        if (!args[0]) return reply('_🙊Please Give Me A Name_\nEg:\n*addplugin AmeenInt*')
+
+      
+        const AmeenXnt = args[0].trim()
+        const filePath = path.join(__dirname, '../plugins', `${AmeenXnt}.js`)
+
+ 
+        if (!quoted || !quoted.text) return reply('_Replay To A Js Code For Octa Bot Plugin🙄._')
+
+
+        fs.writeFileSync(filePath, quoted.text, 'utf8')
+
+        reply(`*Plugin Saved✅*\n\n> Plugin *${AmeenXnt}.js*`)
+    } catch (e) {
+        console.log(e)
+        reply(`_Failed To Save The Plugin:_\nReason:\n ${e.message}`)
+    }
+})
