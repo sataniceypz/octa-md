9a95dec3d607f304cac0d01275300f8761142f53
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-08-26 18:17:37 +0530

    song+video.js

--- plugins/song+video.js
+++ plugins/song+video.js
@@ -1,6 +1,7 @@
 const {eypz , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
+const config = require('../config')
 
 
 eypz({
@@ -28,7 +29,7 @@ let downloadUrl = down.dl_url
 
 //send audio + document message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"Made By Eypz"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption: config.BOT_NAME},{quoted:mek})
 
 
 
@@ -65,7 +66,7 @@ let downloadUrl = down.dl_url
 
 //send video+ document message
 await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName: data.title + ".mp4",caption:"Made By Eypz"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName: data.title + ".mp4",caption:config.BOT_NAME},{quoted:mek})
 
 
 
