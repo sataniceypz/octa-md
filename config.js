4a3acdb3b3668e7988188901b6e8db6917efc769
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-10-25 15:51:44 +0530

    mode

--- config.js
+++ config.js
@@ -4,11 +4,13 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/eypz/1724661875852_gwwMRtTtz.png",
-ALIVE_MSG: process.env.ALIVE_MSG || "> *OCTA ALIVE*",
-OWNER_NAME: process.env.OWNER_NAME || "Eypz",
-BOT_NAME: process.env.BOT_NAME || "OCTA",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    SESSION_ID: process.env.SESSION_ID,
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/eypz/1724661875852_gwwMRtTtz.png",
+    ALIVE_MSG: process.env.ALIVE_MSG || "> *OCTA ALIVE*",
+    OWNER_NAME: process.env.OWNER_NAME || "Eypz",
+    BOT_NAME: process.env.BOT_NAME || "OCTA",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    MODE: process.env.MODE || "public" // Add this line for public/private mode
 };
